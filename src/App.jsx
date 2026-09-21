56a34e0f7085da364866fbfc045bb1ccdd355929
Syed zulqarnain nasir <zulqarnain.nasir@drl.com.pk>
2026-09-21 17:27:35 +0500

    feat(ui): add React Bits ClickSpark interactive particle effect

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,35 @@ import Scanner from './pages/Scanner';
 import Result from './pages/Result';
 import History from './pages/History';
 import About from './pages/About';
+import ClickSpark from './components/reactbits/ClickSpark';
 
 export default function App() {
   return (
     <BrowserRouter>
       <DietaryProvider>
-        <div className="min-h-screen flex flex-col bg-[#FFFFFF] text-[#362211]">
-          <Navbar />
-          <main className="flex-1">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/scanner" element={<Scanner />} />
-              <Route path="/result" element={<Result />} />
-              <Route path="/history" element={<History />} />
-              <Route path="/about" element={<About />} />
-              <Route path="*" element={<Home />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
+        <ClickSpark
+          sparkColor="#2E7D32"
+          sparkSize={12}
+          sparkRadius={20}
+          sparkCount={8}
+          duration={450}
+        >
+          <div className="min-h-screen flex flex-col bg-[#FFFFFF] text-[#362211]">
+            <Navbar />
+            <main className="flex-1">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/scanner" element={<Scanner />} />
+                <Route path="/result" element={<Result />} />
+                <Route path="/history" element={<History />} />
+                <Route path="/about" element={<About />} />
+                <Route path="*" element={<Home />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </ClickSpark>
       </DietaryProvider>
     </BrowserRouter>
   );
